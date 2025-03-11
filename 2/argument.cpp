8882cf59a56c844acb9f1a914739c3c54a1fec98
hjjang777 <hjjang1360@kentech.ac.kr>
2025-03-11 01:14:08 +0000

    practice 2 added

---
+++ 2/argument.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "hello, world!" << endl;
+	cout << "argc: " << argc << endl;
+	cout << "avgv[0]: " << argv[0] << endl;
+	cout << "avgv[1]: " << argv[1] << endl;
+	cout << "avgv[2]: " << argv[2] << endl;
+	cout << "avgv[2]: " << argv[3] << endl;
+	return 0;
+}
